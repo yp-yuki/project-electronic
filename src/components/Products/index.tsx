ae7e3dbbdf8cb7ee86786d5de8e0007ae89ae9c8
yuki <yp.yuki@foxmail.com>
2025-10-12 22:46:39 +0800

    请求优化

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,7 +1,7 @@
 import PageHeader from '../Common/PageHeader'
 import ProductItem from './ProductItem'
 import styles from './index.module.less'
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import { type Product } from '@/types/apiType'
 import { productApi } from '@/api/mock/products'
 import { message, Spin } from 'antd'
@@ -11,14 +11,17 @@ const Products = () => {
     const [loading, setLoading] = useState<boolean>(false)
     const [hasMore, setHasMore] = useState<boolean>(true)
     const [page, setPage] = useState<number>(1)
+    const [errorCount, setErrorCount] = useState<number>(0)
+    const requestSwitch = useRef(false)
     const getProducts = useCallback(async (pageNum: number, isLoadMore: boolean) => {
-        if (loading) return
+        if (loading || !hasMore || requestSwitch.current) return
+        requestSwitch.current = true
         setLoading(true)
         try {
             const limit = isLoadMore ? 6 : 12
             const loadMoreLimit = isLoadMore ? products.length : 0
-            await new Promise(res => setTimeout(res, 500)) //模拟延迟
             const response = await productApi.getProducts({ page: pageNum, limit, loadMoreLimit })
+            await new Promise(res => setTimeout(res, 500)) //模拟延迟
             setHasMore(!response.pagination?.haveNext ? false : true)
             if (isLoadMore) {
                 setProducts(prev => [...prev, ...response.data])
@@ -26,14 +29,21 @@ const Products = () => {
                 setProducts(response.data)
             }
             setPage(pageNum)
+            setErrorCount(0)
         } catch (error) {
-            message.error('获取商品列表失败')
+            setErrorCount(prev => prev + 1)
+            //第一次或每5次请求失败提示一次
+            if(errorCount === 0 || errorCount % 5 === 0){
+                message.error('获取商品列表失败')
+            }
             console.error(error)
         } finally {
             setLoading(false)
+            requestSwitch.current = false
         }
-    }, [loading, products])
+    }, [loading, products.length,errorCount])
     useEffect(() => {
+        //无限加载
         const handleScroll = () => {
             if (loading || !hasMore) return
             const { scrollHeight, scrollTop, clientHeight } = document.documentElement
@@ -45,6 +55,7 @@ const Products = () => {
         return () => window.removeEventListener('scroll', handleScroll)
     }, [page, getProducts, loading, hasMore])
     useEffect(() => {
+        //初始化展示
         getProducts(page, false)
     }, [])
     return <div>
